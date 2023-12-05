4a323ff3c5b66d6946e40621ffd5efb8df05f619
Kevin Armbruster <KevinArmbruster2013@gmail.com>
2023-12-05 14:24:04 +0100

    rescued redesign from stopped container

--- vasopressor/EarlyStopping.py
+++ vasopressor/EarlyStopping.py
@@ -7,15 +7,19 @@ class EarlyStopping:
         MIN = 0
         MAX = 1
     
-    def __init__(self, patience=5, min_delta=0, mode=Mode.MIN):
+    def __init__(self, patience=5, min_delta=0, mode=Mode.MIN, warmup_epochs=0):
         self.patience = patience
         self.min_delta = min_delta
         self.counter = 0
         self.mode = mode
         self.min_max_criterion = float('inf') if mode == EarlyStopping.Mode.MIN else float('-inf')
         self.best_state = 0
+        self.warmup_epochs = warmup_epochs
 
     def check_improvement(self, criterion, state):
+        if state.get("epoch") < self.warmup_epochs:
+            return False
+        
         if self.mode == EarlyStopping.Mode.MIN:
             if criterion < (self.min_max_criterion - self.min_delta):
                 self.min_max_criterion = criterion
@@ -35,10 +39,5 @@ class EarlyStopping:
         if self.counter >= self.patience:
             print(f"Early Stopped")
             self.best_state["early_stopping"] = True
-            return True
-        return False
-    
-    def has_stopped(self):
-        if self.counter >= self.patience:
             return True
         return False
